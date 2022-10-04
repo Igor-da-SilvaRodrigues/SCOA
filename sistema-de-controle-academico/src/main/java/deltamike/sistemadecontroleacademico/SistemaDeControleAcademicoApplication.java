07d694f116d677d786d994919cf9c3ea2051f8fa
Igor-da-SilvaRodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2022-10-04 12:59:25 -0300

    Init
    Spring Initializr

---
+++ sistema-de-controle-academico/src/main/java/deltamike/sistemadecontroleacademico/SistemaDeControleAcademicoApplication.java
@@ -0,0 +1,13 @@
+package deltamike.sistemadecontroleacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeControleAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeControleAcademicoApplication.class, args);
+	}
+
+}
