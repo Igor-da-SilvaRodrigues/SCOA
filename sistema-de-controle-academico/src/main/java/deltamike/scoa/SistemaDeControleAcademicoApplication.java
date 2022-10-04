898197116af6e0bdd99be42f699b91175db7bfd2
Igor-da-SilvaRodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2022-10-04 13:17:32 -0300

    Estrutura definida

--- sistema-de-controle-academico/src/main/java/deltamike/scoa/SistemaDeControleAcademicoApplication.java
+++ sistema-de-controle-academico/src/main/java/deltamike/scoa/SistemaDeControleAcademicoApplication.java
@@ -1,4 +1,4 @@
-package deltamike.sistemadecontroleacademico;
+package deltamike.scoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
