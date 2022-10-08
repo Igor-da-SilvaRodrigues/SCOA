ba465da89ec5668e4222245e2a041fdde0ded1a1
Igor-da-SilvaRodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2022-10-08 10:18:34 -0300

    GET obra

--- sistema-de-controle-academico/src/main/java/deltamike/scoa/controller/biblioteca/obra/ObraController.java
+++ sistema-de-controle-academico/src/main/java/deltamike/scoa/controller/biblioteca/obra/ObraController.java
@@ -6,12 +6,17 @@ package deltamike.scoa.controller.biblioteca.obra;
 
 import deltamike.scoa.dtos.biblioteca.obra.ArtigoDTO;
 import deltamike.scoa.model.biblioteca.obra.ArtigoModel;
+import deltamike.scoa.model.biblioteca.obra.ObraModel;
 import deltamike.scoa.services.biblioteca.obra.ObraService;
+import java.util.List;
+import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,4 +43,17 @@ public class ObraController {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.obraService.save(artigo));
     }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getObraById(@PathVariable Integer id){
+        Optional<ObraModel> obraOptional = this.obraService.getById(id);
+        if (obraOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(obraOptional.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Obra não encontrada");
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<ObraModel>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(this.obraService.getAll());
+    }
 }
