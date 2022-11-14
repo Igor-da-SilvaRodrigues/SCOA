504390987bbaf82d224f23ffcf6c831d5376207c
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2022-11-14 14:32:09 -0300

    Boleto modle e DTO

---
+++ sistema-de-controle-academico/src/main/java/deltamike/scoa/dtos/financeiro/boleto/BoletoDTO.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deltamike.scoa.dtos.financeiro.boleto;
+
+import deltamike.scoa.model.financeiro.mensalidade.MensalidadeModel;
+import java.time.LocalDate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author rodri
+ */
+public class BoletoDTO {
+    @NotNull
+    private LocalDate vencimento;
+    
+    @NotNull
+    private LocalDate data;
+    
+    @NotBlank
+    private String pagador;
+    
+    @NotBlank
+    private String beneficiario;
+    
+    @NotBlank
+    private String linha_digitavel;
+    
+    @NotNull
+    private MensalidadeModel mensalidade;
+
+    
+    public LocalDate getVencimento() {
+        return vencimento;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public String getPagador() {
+        return pagador;
+    }
+
+    public String getBeneficiario() {
+        return beneficiario;
+    }
+
+    public String getLinha_digitavel() {
+        return linha_digitavel;
+    }
+
+    public MensalidadeModel getMensalidade() {
+        return mensalidade;
+    }
+
+    public void setVencimento(LocalDate vencimento) {
+        this.vencimento = vencimento;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public void setPagador(String pagador) {
+        this.pagador = pagador;
+    }
+
+    public void setBeneficiario(String beneficiario) {
+        this.beneficiario = beneficiario;
+    }
+
+    public void setLinha_digitavel(String linha_digitavel) {
+        this.linha_digitavel = linha_digitavel;
+    }
+
+    public void setMensalidade(MensalidadeModel mensalidade) {
+        this.mensalidade = mensalidade;
+    }
+    
+}
---
+++ sistema-de-controle-academico/src/main/java/deltamike/scoa/model/financeiro/boleto/BoletoModel.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deltamike.scoa.model.financeiro.boleto;
+
+import deltamike.scoa.model.financeiro.mensalidade.MensalidadeModel;
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author rodri
+ */
+@Entity
+@Table(name = "boleto")
+public class BoletoModel implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    @Column(nullable = false)
+    private LocalDate vencimento;
+    
+    @Column(nullable = false)
+    private LocalDate data;
+    
+    @Column(nullable = false)
+    private String pagador;
+    
+    @Column(nullable = false)
+    private String beneficiario;
+    
+    @Column(nullable = false)
+    private String linha_digitavel;
+    
+    @ManyToOne
+    private MensalidadeModel mensalidade;
+
+    public BoletoModel(LocalDate vencimento, LocalDate data, String pagador, String beneficiario, String linha_digitavel) {
+        this.vencimento = vencimento;
+        this.data = data;
+        this.pagador = pagador;
+        this.beneficiario = beneficiario;
+        this.linha_digitavel = linha_digitavel;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public LocalDate getVencimento() {
+        return vencimento;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public String getPagador() {
+        return pagador;
+    }
+
+    public String getBeneficiario() {
+        return beneficiario;
+    }
+
+    public String getLinha_digitavel() {
+        return linha_digitavel;
+    }
+
+    public MensalidadeModel getMensalidade() {
+        return mensalidade;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setVencimento(LocalDate vencimento) {
+        this.vencimento = vencimento;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public void setPagador(String pagador) {
+        this.pagador = pagador;
+    }
+
+    public void setBeneficiario(String beneficiario) {
+        this.beneficiario = beneficiario;
+    }
+
+    public void setLinha_digitavel(String linha_digitavel) {
+        this.linha_digitavel = linha_digitavel;
+    }
+
+    public void setMensalidade(MensalidadeModel mensalidade) {
+        this.mensalidade = mensalidade;
+    }
+    
+}
