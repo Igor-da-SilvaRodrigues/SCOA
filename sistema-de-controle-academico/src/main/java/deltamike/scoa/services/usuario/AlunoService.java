afdef5b09681538ecc1ff9c9ecaeac0a1a9cdd05
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2022-12-01 14:31:43 -0300

    AlunoService agora usa ScoaService.

--- sistema-de-controle-academico/src/main/java/deltamike/scoa/services/usuario/AlunoService.java
+++ sistema-de-controle-academico/src/main/java/deltamike/scoa/services/usuario/AlunoService.java
@@ -6,9 +6,11 @@ package deltamike.scoa.services.usuario;
 
 import deltamike.scoa.model.usuario.AlunoModel;
 import deltamike.scoa.repositories.usuario.AlunoRepository;
+import deltamike.scoa.services.ScoaService;
 import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,45 +18,12 @@ import org.springframework.stereotype.Service;
  * @author rodri
  */
 @Service
-public class AlunoService {
-    final AlunoRepository alunoRepository;
+public class AlunoService extends ScoaService<AlunoModel, Integer, AlunoRepository>{
     final UsuarioService usuarioService;
 
-    public AlunoService(AlunoRepository alunoRepository, UsuarioService usuarioService) {
-        this.alunoRepository = alunoRepository;
+    public AlunoService(UsuarioService usuarioService, AlunoRepository repository) {
+        super(repository);
         this.usuarioService = usuarioService;
-    }
-    
-    
-    
-    @Transactional
-    public AlunoModel save(AlunoModel u){        
-        return this.alunoRepository.saveAndFlush(u);
-    }
-    
-    @Transactional
-    public void delete(AlunoModel u){
-        this.alunoRepository.delete(u);
-    }
-    
-    public void deleteById(Integer id){
-        Optional<AlunoModel> userOptional = this.getById(id);
-        
-        if (userOptional.isPresent()){
-            this.delete(userOptional.get());
-        }
-    }
-    
-    public boolean existsById(Integer id){
-        return this.alunoRepository.existsById(id);
-    }
-    
-    public List<AlunoModel> getAll(){
-        return this.alunoRepository.findAll();
-    }
-    
-    public Optional<AlunoModel> getById(Integer id){
-        return this.alunoRepository.findById(id);
     }
 
     public UsuarioService getUsuarioService() {
