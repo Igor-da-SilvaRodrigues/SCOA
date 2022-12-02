12cbb626bbfae3a503be53bc69733ca035e92e51
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2022-12-01 21:35:57 -0300

    PUT coordenador_usuario

--- sistema-de-controle-academico/src/main/java/deltamike/scoa/controller/usuario/CoordenadorController.java
+++ sistema-de-controle-academico/src/main/java/deltamike/scoa/controller/usuario/CoordenadorController.java
@@ -6,6 +6,7 @@ package deltamike.scoa.controller.usuario;
 
 import deltamike.scoa.dtos.usuario.CoordenadorDTO;
 import deltamike.scoa.model.usuario.CoordenadorModel;
+import deltamike.scoa.model.usuario.UsuarioModel;
 import deltamike.scoa.services.usuario.CoordenadorService;
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,12 +61,41 @@ public class CoordenadorController {
         return ResponseEntity.status(HttpStatus.OK).body(coordenadorModel);
     }
     
+    @PutMapping("/{idCoordenador}/usuario/{idUsuario}")
+    public ResponseEntity<Object> colocarCoordenadorEmUsuario(@PathVariable Integer idCoordenador, @PathVariable String idUsuario){
+        Optional<CoordenadorModel> coordenadorOptional = this.coordenadorService.getById(idCoordenador);
+        Optional<UsuarioModel> usuarioOptional = this.coordenadorService.getUsuarioService().getById(idUsuario);
+        
+        if(coordenadorOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Coordenador não encontrado");
+        }
+        
+        if(usuarioOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
+        }
+        
+        CoordenadorModel coordenadorModel = coordenadorOptional.get();
+        UsuarioModel usuarioModel = usuarioOptional.get();
+        
+        coordenadorModel.setUsuario(usuarioModel);
+        
+        CoordenadorModel retorno = this.coordenadorService.save(coordenadorModel);
+        
+        return ResponseEntity.status(HttpStatus.OK).body(retorno);
+    }
+    
     @GetMapping
     public ResponseEntity<List<CoordenadorModel>> getAll(){
         return ResponseEntity.status(HttpStatus.OK).body(this.coordenadorService.getAll());
     }
 
     public ResponseEntity<Object> getById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Optional<CoordenadorModel> coordenadorOptional = this.coordenadorService.getById(id);
+        
+        if(coordenadorOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Coordenador não encontrado");
+        }
+        
+        return ResponseEntity.status(HttpStatus.OK).body(coordenadorOptional.get());
     }
 }
