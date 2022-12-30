c6cdb43649c502e959504146e0ea0438f8474b5e
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2022-12-29 22:48:23 -0300

    Adicionado Jframe pra view
    O Jframe contem uma variável que armazena o contexto da aplicação spring, que pode ser utilizada para acessar os controllers e por fim o banco de dados.

--- sistema-de-controle-academico/src/main/java/deltamike/scoa/SistemaDeControleAcademicoApplication.java
+++ sistema-de-controle-academico/src/main/java/deltamike/scoa/SistemaDeControleAcademicoApplication.java
@@ -2,12 +2,16 @@ package deltamike.scoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootApplication
-public class SistemaDeControleAcademicoApplication {
-
+public class SistemaDeControleAcademicoApplication{
+        
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaDeControleAcademicoApplication.class, args);
-	}
+            ConfigurableApplicationContext context = SpringApplication.run(SistemaDeControleAcademicoApplication.class, args);
+            
+        }
 
+       
 }
