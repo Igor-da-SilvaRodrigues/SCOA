906476450537ce8f5620103bff617f679409fa28
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2025-01-11 19:49:35 -0300

    Remove drop schema from sql script

--- SQL/academico.sql
+++ SQL/academico.sql
@@ -10,11 +10,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema Scoa
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `Scoa` ;
-
--- -----------------------------------------------------
--- Schema Scoa
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `Scoa` DEFAULT CHARACTER SET utf8 ;
 USE `Scoa` ;
 
--- SQL/biblioteca.sql
+++ SQL/biblioteca.sql
@@ -10,10 +10,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema Scoa
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema Scoa
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `Scoa` DEFAULT CHARACTER SET utf8 ;
 USE `Scoa` ;
 
