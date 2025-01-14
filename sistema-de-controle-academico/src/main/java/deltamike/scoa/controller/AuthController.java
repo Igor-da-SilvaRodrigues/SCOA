6e448ccf96eb03333d2913287cd4afd5ccba16dc
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2025-01-14 16:22:50 -0300

    Login

---
+++ sistema-de-controle-academico/src/main/java/deltamike/scoa/controller/AuthController.java
@@ -0,0 +1,37 @@
+package deltamike.scoa.controller;
+
+import deltamike.scoa.dtos.LoginRequestDto;
+import deltamike.scoa.model.usuario.UsuarioModel;
+import deltamike.scoa.security.services.TokenService;
+import deltamike.scoa.services.usuario.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UsuarioService usuarioService;
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid LoginRequestDto dto) {
+        var internalToken = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
+        var auth = this.authenticationManager.authenticate(internalToken);
+
+        String token = tokenService.generateToken((UsuarioModel) auth.getPrincipal());
+        return ResponseEntity.ok(token);
+    }
+
+}
--- sistema-de-controle-academico/src/main/java/deltamike/scoa/security/SecurityConfig.java
+++ sistema-de-controle-academico/src/main/java/deltamike/scoa/security/SecurityConfig.java
@@ -2,12 +2,17 @@ package deltamike.scoa.security;
 
 import deltamike.scoa.security.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,20 +22,29 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     @Autowired
-    private AuthService authService;
-    @Autowired
     private SecurityFilter filter;
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
